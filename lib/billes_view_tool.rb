13a1786365b5e15ba1dc115475027bc3e18bb35b
Mark Billes <mark.billes@web.de>
2017-08-21 23:21:24 +0200

    initial commit

---
+++ lib/billes_view_tool.rb
@@ -0,0 +1,6 @@
+require "billes_view_tool/version"
+require "billes_view_tool/renderer"
+
+module BillesViewTool
+  # Your code goes here...
+end
---
+++ lib/billes_view_tool/version.rb
@@ -0,0 +1,3 @@
+module BillesViewTool
+  VERSION = "0.1.0"
+end
